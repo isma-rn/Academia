596d9e713f380727845c559c3ceea082184eae64
Juan Ismael Rosas Nava <jismael-rn@outlook.com>
2024-05-28 14:44:11 -0600

    Feat: registro models al panel admin

--- GestionAcademia/admin.py
+++ GestionAcademia/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from GestionAcademia.models import Afinidad,Grimorio,Estudiante,Estatus,Solicitud
 
-# Register your models here.
+@admin.register(Afinidad)
+class AfinidadAdmin(admin.ModelAdmin):
+    list_display=['id','nombre']
+
+@admin.register(Grimorio)
+class GrimorioAdmin(admin.ModelAdmin):
+    list_display=['id','nombre']
+
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display=['id','nombre','apellido','identificacion', 'afinidad', 'grimorio']
+
+@admin.register(Estatus)
+class EstatusAdmin(admin.ModelAdmin):
+    list_display=['id','nombre']
+
+@admin.register(Solicitud)
+class SolicitudAdmin(admin.ModelAdmin):
+    list_display=['id','creacion','ultimaModificacion','estudiante', 'estatus']
