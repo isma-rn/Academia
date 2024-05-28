a029d4372d04bd364534c7aff44e61b0a47552b1
Juan Ismael Rosas Nava <jismael-rn@outlook.com>
2024-05-28 12:27:27 -0600

    Feat: base de datos

---
+++ GestionAcademia/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionacademiaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GestionAcademia'
---
+++ GestionAcademia/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Afinidad(models.Model):
+    nombre=models.CharField(max_length=20)
+
+class Grimorio(models.Model):
+    nombre=models.CharField(max_length=20)
+    numeroHojas=models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre=models.CharField(max_length=20)
+    apellido=models.CharField(max_length=20)
+    identificacion=models.CharField(max_length=10)
+    edad=models.IntegerField()
+    afinidad=models.ForeignKey(Afinidad, on_delete=models.CASCADE)
+    grimorio=models.ForeignKey(Grimorio, on_delete=models.CASCADE)
+
+class Estatus(models.Model):
+    nombre=models.CharField(max_length=20)
+
+class Solicitud(models.Model):
+    creacion=models.DateTimeField()
+    ultimaModificacion=models.DateTimeField(null=True,blank=True)
+    estudiante=models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    estatus=models.ForeignKey(Estatus, on_delete=models.CASCADE)
